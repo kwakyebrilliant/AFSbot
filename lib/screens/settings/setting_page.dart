6b54c06e97513e73b8c756b86e653ba8cc671c20
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-23 21:26:49 +0000

    setting page updates

--- lib/screens/settings/setting_page.dart
+++ lib/screens/settings/setting_page.dart
@@ -36,6 +36,67 @@ class _SettingPageState extends State<SettingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Theme.of(context).colorScheme.background,
+            expandedHeight: 140.0,
+            collapsedHeight: 30.0,
+            toolbarHeight: 30.0,
+            floating: false,
+            pinned: true,
+            elevation: 0,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              background: Image.asset(
+                "assets/images/chat.png",
+                fit: BoxFit.cover,
+              ),
+              title: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.blueGrey,
+                        ),
+                        child: Icon(
+                          Icons.person,
+                          size: 40.0,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(
+                      width: 5.0,
+                    ),
+
+                    //user first name here
+                    Container(
+                      color: Theme.of(context)
+                          .colorScheme
+                          .background
+                          .withOpacity(0.7),
+                      child: Text(
+                        '@brilliant',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.inversePrimary,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
