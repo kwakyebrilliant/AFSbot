2dd26e864a70f9b32bd3d194baef94dd4d1d5ce9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-23 22:11:24 +0000

    setting page updates

--- lib/screens/settings/setting_page.dart
+++ lib/screens/settings/setting_page.dart
@@ -94,7 +94,151 @@ class _SettingPageState extends State<SettingPage> {
                 ),
               ),
             ),
-          )
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //Top cards here
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 30.0, left: 15.0, right: 15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        //container for the number of prompts and its icon
+                        Container(
+                          height: 110.0,
+                          width: 110.0,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.background,
+                            borderRadius: BorderRadius.circular(16.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .inversePrimary,
+                                offset: const Offset(1, 1),
+                                blurRadius: 2.0,
+                              ),
+                            ],
+                          ),
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 25.0),
+                                  child: Icon(
+                                    Icons.edit_rounded,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .inversePrimary,
+                                    size: 30,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 10.0),
+                                  child: Text(
+                                    '316',
+                                    style: TextStyle(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .inversePrimary,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        //themes here
+                        Container(
+                          height: 110.0,
+                          width: 110.0,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(16.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: const Color(0xFF17203A).withOpacity(0.3),
+                                offset: const Offset(2, 5),
+                                blurRadius: 10.0,
+                              ),
+                            ],
+                          ),
+                          child: const Center(
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(top: 25.0),
+                                  child: Icon(
+                                    Icons.toggle_on_rounded,
+                                    color: Colors.black,
+                                    size: 30,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(top: 10.0),
+                                  child: Text(
+                                    'Themes',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        //notifications
+                        Container(
+                          height: 110.0,
+                          width: 110.0,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(16.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: const Color(0xFF17203A).withOpacity(0.3),
+                                offset: const Offset(2, 5),
+                                blurRadius: 10.0,
+                              ),
+                            ],
+                          ),
+                          child: const Center(
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(top: 25.0),
+                                  child: Icon(
+                                    Icons.notifications,
+                                    color: Colors.black,
+                                    size: 30,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(top: 10.0),
+                                  child: Text(
+                                    'Notification',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
