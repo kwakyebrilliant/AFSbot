aa6d6bf7b346bc96adbfcd77cabb080003313a89
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-23 21:15:31 +0000

    setting page

--- lib/screens/settings/setting_page.dart
+++ lib/screens/settings/setting_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({super.key});
@@ -8,8 +9,33 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
+  bool isSwitched = false;
+  bool isDarkMode = false;
+
+  String? downloadURL;
+
+  @override
+  void initState() {
+    super.initState();
+    loadSwitchState();
+  }
+
+  void loadSwitchState() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    });
+  }
+
+  void saveSwitchState() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkMode', isDarkMode); // Save the switch state
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+    );
   }
 }
