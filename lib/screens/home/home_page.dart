72b61dafbc32e6d6536c6dc5b8f69ce612e5b5fd
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-24 10:00:51 +0000

    home updates

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -10,6 +10,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+      ),
+    );
   }
 }
