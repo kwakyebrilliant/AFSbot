d75ebf44d351859f5a7c0089c1d87e3bd33fabd8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-24 08:29:58 +0000

    images updates

--- lib/screens/settings/about_page.dart
+++ lib/screens/settings/about_page.dart
@@ -4,8 +4,30 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
-        title: Text('About'),
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        elevation: 0,
+        title: Text(
+          'About',
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.inversePrimary,
+            fontSize: 20.0,
+          ),
+        ),
+        centerTitle: true,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Container(
+            child: Icon(
+              Icons.arrow_left_rounded,
+              color: Theme.of(context).colorScheme.inversePrimary,
+              size: 50.0,
+            ),
+          ),
+        ),
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(20.0),
