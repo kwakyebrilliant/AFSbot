5fc3979a78cffcda7d3c3db3edc4d104cb76b4d3
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-24 12:44:48 +0000

    prompt details updates

--- lib/screens/prompt/promptdeatils_page.dart
+++ lib/screens/prompt/promptdeatils_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class PromptDetailsPage extends StatefulWidget {
   const PromptDetailsPage({super.key});
@@ -10,6 +11,39 @@ class PromptDetailsPage extends StatefulWidget {
 class _PromptDetailsPageState extends State<PromptDetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        elevation: 0,
+        title: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(7.0),
+                child: CircleAvatar(
+                  radius: 20,
+                  backgroundImage: const AssetImage('assets/images/logo.png'),
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+
+              // afs chatbot text here
+              const Padding(
+                padding: EdgeInsets.only(left: 50.0),
+                child: Text(
+                  'Chat with AFSbot',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
