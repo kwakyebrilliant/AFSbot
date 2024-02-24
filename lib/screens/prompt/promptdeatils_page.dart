9ea5c6079211c5b5076b853d70d57c421ed0bf46
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-24 12:42:25 +0000

    prompt deatils added

---
+++ lib/screens/prompt/promptdeatils_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class PromptDetailsPage extends StatefulWidget {
+  const PromptDetailsPage({super.key});
+
+  @override
+  State<PromptDetailsPage> createState() => _PromptDetailsPageState();
+}
+
+class _PromptDetailsPageState extends State<PromptDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
