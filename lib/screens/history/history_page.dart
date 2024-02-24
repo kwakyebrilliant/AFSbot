2b7bd3c040654d323bcccef79a817a1436ed0f8a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-24 21:51:19 +0000

    history updates

--- lib/screens/history/history_page.dart
+++ lib/screens/history/history_page.dart
@@ -1,5 +1,4 @@
 import 'package:afsbot/screens/prompt/promptdeatils_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HistoryPage extends StatefulWidget {
