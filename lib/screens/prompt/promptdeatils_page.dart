e3797f633af1f6acd70e86faf3b60bd499cf85a7
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-24 13:06:52 +0000

    prompt details updates

--- lib/screens/prompt/promptdeatils_page.dart
+++ lib/screens/prompt/promptdeatils_page.dart
@@ -9,6 +9,8 @@ class PromptDetailsPage extends StatefulWidget {
 }
 
 class _PromptDetailsPageState extends State<PromptDetailsPage> {
+  bool isFavoriteActive = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,29 +18,62 @@ class _PromptDetailsPageState extends State<PromptDetailsPage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
         elevation: 0,
+        centerTitle: true,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Icon(
+            Icons.arrow_left_rounded,
+            color: Theme.of(context).colorScheme.inversePrimary,
+            size: 50.0,
+          ),
+        ),
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Padding(
               padding: const EdgeInsets.all(7.0),
-              child: CircleAvatar(
-                radius: 20,
-                backgroundImage: const AssetImage('assets/images/logo.png'),
-                backgroundColor: Theme.of(context).colorScheme.secondary,
-              ),
-            ),
+              child: Container(
+                padding: const EdgeInsets.all(7.0),
+                height: 42.0,
+                width: 42.0,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  borderRadius: BorderRadius.circular(60.0),
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isFavoriteActive = !isFavoriteActive;
+                    });
 
-            // afs chatbot text here
-            const Padding(
-              padding: EdgeInsets.only(left: 50.0),
-              child: Text(
-                'Chat with AFSbot',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.0,
+                    // Show Snackbar based on the favorite status
+                    final snackBarMessage = isFavoriteActive
+                        ? 'Added as favorite'
+                        : 'Removed from favorite';
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          snackBarMessage,
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.background,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  child: Icon(
+                    isFavoriteActive
+                        ? Icons.favorite_outlined
+                        : Icons.favorite_outline_rounded,
+                    color: Theme.of(context).colorScheme.background,
+                    size: 25.0,
+                  ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
