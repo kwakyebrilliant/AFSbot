f1876440831d8d56a378ab0226a1a4b2c66a573f
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-24 00:03:44 +0000

    settings updates

--- lib/screens/settings/setting_page.dart
+++ lib/screens/settings/setting_page.dart
@@ -43,9 +43,9 @@ class _SettingPageState extends State<SettingPage> {
           slivers: [
             SliverAppBar(
               backgroundColor: Theme.of(context).colorScheme.background,
-              expandedHeight: 140.0,
-              collapsedHeight: 30.0,
-              toolbarHeight: 30.0,
+              expandedHeight: 100.0,
+              // collapsedHeight: 30.0,
+              // toolbarHeight: 30.0,
               floating: false,
               pinned: true,
               elevation: 0,
@@ -59,25 +59,6 @@ class _SettingPageState extends State<SettingPage> {
                   padding: const EdgeInsets.only(left: 8.0),
                   child: Row(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.blueGrey,
-                          ),
-                          child: Icon(
-                            Icons.person,
-                            size: 40.0,
-                            color: Theme.of(context).colorScheme.primary,
-                          ),
-                        ),
-                      ),
-
-                      const SizedBox(
-                        width: 5.0,
-                      ),
-
                       //username here
                       Container(
                         color: Theme.of(context)
