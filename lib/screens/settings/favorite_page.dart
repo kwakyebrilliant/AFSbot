564cb27691b62f694afc6ee720ba1cf60d6de754
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-24 09:03:37 +0000

    favorite page updates

--- lib/screens/settings/favorite_page.dart
+++ lib/screens/settings/favorite_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:appbar_animated/appbar_animated.dart';
 
 class FavoritePage extends StatefulWidget {
   const FavoritePage({super.key});
@@ -10,6 +11,151 @@ class FavoritePage extends StatefulWidget {
 class _FavoritePageState extends State<FavoritePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.inverseSurface,
+      body: ScaffoldLayoutBuilder(
+        backgroundColorAppBar:
+            const ColorBuilder(Colors.transparent, Colors.white),
+        textColorAppBar: const ColorBuilder(Colors.white),
+        appBarBuilder: _appBar,
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              const Positioned(
+                top: 150.0,
+                left: 50.0,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "Share ShareBible",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Your greatest gift",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.30,
+                    ),
+                    height: 600,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(40),
+                      ),
+                      color: Theme.of(context).colorScheme.background,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 80.0),
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: const Text(
+                                    "Help in sharing the Gospel to the many",
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  child: const Text(
+                                    "people around you and far.",
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  child: const Text(
+                                    "Thank you!",
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 30.0),
+                                  child: TextButton.icon(
+                                    style: TextButton.styleFrom(
+                                      backgroundColor: Theme.of(context)
+                                          .colorScheme
+                                          .inversePrimary,
+                                    ),
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.share_rounded,
+                                    ),
+                                    label: const Text(
+                                      'Share ShareBible',
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 250.0),
+                                  child: Container(
+                                    child: const Text(
+                                      "Jesus love you.",
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _appBar(BuildContext context, ColorAnimated colorAnimated) {
+    return AppBar(
+      backgroundColor: colorAnimated.background,
+      elevation: 0,
+      leading: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pop();
+        },
+        child: Padding(
+          padding: const EdgeInsets.only(left: 13.0, bottom: 13.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.background,
+              border: Border.all(
+                  color: Theme.of(context).colorScheme.secondary, width: 1.0),
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Icon(
+              Icons.arrow_left_rounded,
+              color: Theme.of(context).colorScheme.inversePrimary,
+              size: 40.0,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
