87a5ae3a66aeb882953b76ccce11ea3840a5708e
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-24 12:39:01 +0000

    prompt updates

--- lib/screens/prompt/prompt_page.dart
+++ lib/screens/prompt/prompt_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:appbar_animated/appbar_animated.dart';
 
 class PromptPage extends StatefulWidget {
   const PromptPage({Key? key}) : super(key: key);
@@ -14,82 +13,80 @@ class _PromptPageState extends State<PromptPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: ScaffoldLayoutBuilder(
-        backgroundColorAppBar:
-            const ColorBuilder(Colors.transparent, Colors.white),
-        textColorAppBar: const ColorBuilder(Colors.white),
-        appBarBuilder: _appBar,
-        child: Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height,
-                      padding: const EdgeInsets.only(bottom: 20.0),
-                      margin: EdgeInsets.only(
-                        top: MediaQuery.of(context).size.height * 0.20,
-                      ),
-                      decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.vertical(
-                          top: Radius.circular(40),
-                        ),
-                        color: Theme.of(context).colorScheme.inverseSurface,
-                      ),
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            width: MediaQuery.of(context).size.width,
-                            child: Column(
-                              children: [
-                                //circle avatar displaying the logo
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 120.0),
-                                  child: CircleAvatar(
-                                    radius: 80,
-                                    backgroundImage: const AssetImage(
-                                        'assets/images/logo.png'),
-                                    backgroundColor: Theme.of(context)
-                                        .colorScheme
-                                        .background,
-                                  ),
-                                ),
-
-                                //Text displaying how
-                                const Padding(
-                                  padding: EdgeInsets.only(top: 80.0),
-                                  child: Text(
-                                    'How can I help you Today?',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: 34,
-                                      fontWeight: FontWeight.bold,
-                                      wordSpacing: -2,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        elevation: 0,
+        centerTitle: true,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Container(
+            child: Icon(
+              Icons.arrow_left_rounded,
+              color: Theme.of(context).colorScheme.inversePrimary,
+              size: 50.0,
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).cardColor,
-                ),
-                child: _buildTextComposer(),
-              ),
-            ),
-          ],
+          ),
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          children: [
+                            //circle avatar displaying the logo
+                            Padding(
+                              padding: const EdgeInsets.only(top: 120.0),
+                              child: CircleAvatar(
+                                radius: 80,
+                                backgroundImage:
+                                    const AssetImage('assets/images/logo.png'),
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .inverseSurface,
+                              ),
+                            ),
+
+                            //Text displaying how
+                            const Padding(
+                              padding: EdgeInsets.only(top: 80.0),
+                              child: Text(
+                                'How can I help you Today?',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 34,
+                                  fontWeight: FontWeight.bold,
+                                  wordSpacing: -2,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: _buildTextComposer(),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -117,34 +114,6 @@ class _PromptPageState extends State<PromptPage> {
               onPressed: () {},
             ),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget _appBar(BuildContext context, ColorAnimated colorAnimated) {
-    return AppBar(
-      backgroundColor: colorAnimated.background,
-      elevation: 0,
-      leading: GestureDetector(
-        onTap: () {
-          Navigator.of(context).pop();
-        },
-        child: Padding(
-          padding: const EdgeInsets.only(left: 13.0, bottom: 13.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.background,
-              border: Border.all(
-                  color: Theme.of(context).colorScheme.secondary, width: 1.0),
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: Icon(
-              Icons.arrow_left_rounded,
-              color: Theme.of(context).colorScheme.inversePrimary,
-              size: 40.0,
-            ),
-          ),
         ),
       ),
     );
