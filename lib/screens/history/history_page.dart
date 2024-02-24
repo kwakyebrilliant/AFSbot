de63132db75c88d4f9e78bf7ab7e6616c1fbb475
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-24 00:09:01 +0000

    history updates

--- lib/screens/history/history_page.dart
+++ lib/screens/history/history_page.dart
@@ -10,39 +10,64 @@ class HistoryPage extends StatefulWidget {
 class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: Theme.of(context).colorScheme.background,
-            expandedHeight: 100.0,
-            collapsedHeight: 30.0,
-            toolbarHeight: 30.0,
-            floating: false,
-            pinned: true,
-            elevation: 0,
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              // background: Image.asset(
-              //   "assets/images/wallpaper.png",
-              //   fit: BoxFit.cover,
-              // ),
-              title: Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: Row(
-                  children: [],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: Theme.of(context).colorScheme.background,
+              expandedHeight: 100.0,
+              floating: false,
+              pinned: true,
+              elevation: 0,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                // background: Image.asset(
+                //   "assets/images/wallpaper.png",
+                //   fit: BoxFit.cover,
+                // ),
+                title: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.inversePrimary,
+                          ),
+                          decoration: InputDecoration(
+                            hintText: 'Search',
+                            hintStyle: TextStyle(
+                              color:
+                                  Theme.of(context).colorScheme.inverseSurface,
+                            ),
+                            prefixIcon: Padding(
+                              padding:
+                                  const EdgeInsets.only(right: 8.0, left: 8.0),
+                              child: Icon(
+                                Icons.search_rounded,
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .inverseSurface,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [],
+            SliverToBoxAdapter(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
