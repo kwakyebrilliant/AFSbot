e59f8c6f00beebf11f2090b3523cb1fe8723a1ad
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-24 12:46:33 +0000

    prompt details updates

--- lib/screens/prompt/promptdeatils_page.dart
+++ lib/screens/prompt/promptdeatils_page.dart
@@ -16,32 +16,30 @@ class _PromptDetailsPageState extends State<PromptDetailsPage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
         elevation: 0,
-        title: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(7.0),
-                child: CircleAvatar(
-                  radius: 20,
-                  backgroundImage: const AssetImage('assets/images/logo.png'),
-                  backgroundColor: Theme.of(context).colorScheme.secondary,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(7.0),
+              child: CircleAvatar(
+                radius: 20,
+                backgroundImage: const AssetImage('assets/images/logo.png'),
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+              ),
+            ),
+
+            // afs chatbot text here
+            const Padding(
+              padding: EdgeInsets.only(left: 50.0),
+              child: Text(
+                'Chat with AFSbot',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
                 ),
               ),
-
-              // afs chatbot text here
-              const Padding(
-                padding: EdgeInsets.only(left: 50.0),
-                child: Text(
-                  'Chat with AFSbot',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                  ),
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
