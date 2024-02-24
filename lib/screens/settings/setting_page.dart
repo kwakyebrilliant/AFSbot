9412b28558af342dc559cbd59f4a442820542d01
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-24 21:37:28 +0000

    settings updates

--- lib/screens/settings/setting_page.dart
+++ lib/screens/settings/setting_page.dart
@@ -1,10 +1,7 @@
 import 'package:afsbot/screens/settings/about_page.dart';
 import 'package:afsbot/screens/settings/favorite_page.dart';
 import 'package:afsbot/theme/theme_notifier.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:afsbot/screens/settings/faq_page.dart';
