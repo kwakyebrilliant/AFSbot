9a8cb8689551bb4c9cefff2a65ba27b5f463a9b1
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-26 03:02:40 +0000

    login updates

--- lib/users/login_page.dart
+++ lib/users/login_page.dart
@@ -72,6 +72,13 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
